68b14d5205254248ac447d648259d8865692e993
George Waller <george.waller3@gmail.com>
2020-06-22 13:41:37 +0100

    updated AddPlayer logic

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 
 namespace LinkedList
 {
@@ -8,7 +9,7 @@ namespace LinkedList
     public string Name { get; }
     public int Pointer { get; set; }
 
-    public Player(string newName, int newPointer)
+    public Player(string newName, int newPointer = -1)
     {
         this.Name = newName;
         this.Pointer = newPointer;
@@ -27,20 +28,43 @@ namespace LinkedList
             }
         }
 
-        static void GetAllPlayersInOrder(ref List<Player> list, int index)
+        static List<Player> GetAllPlayersInOrder(ref List<Player> list, int index = 2, bool console = true, int limit = -1)
         {
-            for (int i = 0; i < list.Count; i++)
+            List<Player> result = new List<Player>();
+            int count = list.Count;
+            if (!console)
             {
-                Console.WriteLine($"{i}\t{list[index].Name}");
-                index = list[index].Pointer;
-                if (index == -1) {return;}
+                if (limit != -1)
+                {
+                    count = limit;
+                }
             }
+
+            for (int i = 0; i < count; i++)
+            {
+                if (console) {Console.WriteLine($"{i}\t{list[index].Name}");}
+                else {result.Add(list[index]);}
+                
+                index = list[index].Pointer;
+                if (index == -1)
+                {
+                    if (console)
+                    {
+                        Console.WriteLine("\n");
+                        return null;
+                    }
+                    else {return result;}
+                }
+            }
+            return null;
         }
 
-        static void AddPlayer(ref List<Player> list, Player playerAfter, Player newPlayer)
+        static void AddPlayer(ref List<Player> list, int position, Player newPlayer)
         {
-            newPlayer.Pointer = playerAfter.Pointer;
-            playerAfter.Pointer = list.IndexOf(newPlayer);
+            List<Player> orderedList = GetAllPlayersInOrder(ref list, console: false);
+
+            newPlayer.Pointer = list[list.IndexOf(orderedList[position - 1])].Pointer;
+            list[list.IndexOf(orderedList[position - 1])].Pointer = list.IndexOf(newPlayer);
         }
 
         static void Main(string[] args)
@@ -52,12 +76,23 @@ namespace LinkedList
             Player p5 = new Player("George", 1);
 
             linkedList.AddRange(new Player[]{p1, p2, p3, p4, p5});
+            GetAllPlayersInOrder(ref linkedList);
 
-            Player p6 = new Player("Mr Tattam", 3);
+            Player p6 = new Player("Mr Tattam");
             linkedList.Add(p6);
-            AddPlayer(ref linkedList, p1, p6);
+            AddPlayer(ref linkedList, 4, p6);
+            GetAllPlayersInOrder(ref linkedList);
+
+            Player p7 = new Player("Ruben");
+            linkedList.Add(p7);
+            AddPlayer(ref linkedList, 4, p7);
+            GetAllPlayersInOrder(ref linkedList);
             
-            GetAllPlayersInOrder(ref linkedList, 2);
+            Player p8 = new Player("Tom");
+            linkedList.Add(p8);
+            AddPlayer(ref linkedList, 4, p8);
+            
+            GetAllPlayersInOrder(ref linkedList);
             //PrintAllPlayers(linkedList);
         }
     }
